007699c8b093296fcb3a1d82842c46b3fa1c0f1f
Rory Gracey <40322132@live.napier.ac.uk>
2020-02-25 16:31:42 +0000

    Quaratine networking started

--- src/main/java/QuarantineNetworkManager.groovy
+++ src/main/java/QuarantineNetworkManager.groovy
@@ -1,21 +1,33 @@
 import jcsp.lang.*
 import groovyJCSP.*
+import jcsp.net2.NetChannel
+import jcsp.net2.Node
+import jcsp.net2.tcpip.TCPIPNodeAddress
+
 import java.util.Random
 
 class QuarantineNetworkManager implements CSProcess {
     ChannelInput fromNetworkManager
     ChannelInputList fromChecker
     ChannelOutputList toChecker
-     ChannelOutput toBuffer
+    ChannelOutput toBuffer
     ChannelOutput jobRequest
     void run(){
         sleep(1000)
         println('started')
+        def controllerIP = '127.0.0.1'
         def avCheckers = []
         println(avCheckers)
         for(i in 1 .. toChecker.size() - 2){
             avCheckers << i
         }
+        def nodeAddr = new TCPIPNodeAddress(4000)
+        Node.getInstance().init(nodeAddr)
+
+        def toNM = new TCPIPNodeAddress(controllerIP, 3000)
+        def toNMChan = NetChannel.any2net(toNM, 50)
+        def fromNM = NetChannel.net2one()
+        def fromNMLoc = fromNM.getLocation()
 
         while (true){
             jobRequest.write('RequestJob')
