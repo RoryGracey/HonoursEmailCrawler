d9acacda9c0f7d7acf186d184745adfdfb2c6a6c
Rory Gracey <40322132@live.napier.ac.uk>
2020-02-25 18:10:00 +0000

    Trying to implement network buffer net. Still has errors...

--- src/main/java/Checker.groovy
+++ src/main/java/Checker.groovy
@@ -2,6 +2,9 @@ import jcsp.lang.CSProcess
 import jcsp.lang.ChannelInput
 import jcsp.lang.ChannelOutput
 import groovyJCSP.MobileAgent
+import jcsp.net2.NetChannel
+import jcsp.net2.tcpip.TCPIPNodeAddress
+
 import java.net.InetAddress
 
 class Checker implements CSProcess{
@@ -11,6 +14,9 @@ class Checker implements CSProcess{
     def nodeID
     void run(){
         while(true) {
+            def controllerIP = '146.176.75.9'
+            def toNM = new TCPIPNodeAddress(controllerIP, 3003)
+            def toNMChan = NetChannel.any2net(toNM, 50)
             def job = channelInput.read()
             def connection =  new URL(job.toString()).openConnection()
             Scanner scanner = new Scanner(connection.getInputStream())
@@ -21,7 +27,7 @@ class Checker implements CSProcess{
             //println(result)
             // println(nodeID + ' did this job: ' + job)
             if(result == 'GOOD') {
-                toNetworkBuffer.write('Node ' + nodeID + ': ' + result)
+                toNMChan.write('Node ' + nodeID + ': ' + result)
                 sleep(1000)
                 println('Node ' + nodeID + ' got bad result: ' + result)
             }else if(result == 'BAD'){
--- src/main/java/NMBuffer.groovy
+++ src/main/java/NMBuffer.groovy
@@ -1,11 +1,17 @@
 import jcsp.lang.*
+import jcsp.net2.NetChannel
+import jcsp.net2.Node
+import jcsp.net2.tcpip.TCPIPNodeAddress
 
 class NMBuffer implements CSProcess{
     ChannelInput fromCheckerProcesses
     ChannelOutput toReceiver
     void run() {
     while(true){
-        def result = fromCheckerProcesses.read()
+        def nodeAddr = new TCPIPNodeAddress(3003)
+        Node.getInstance().init(nodeAddr)
+        def fromCheckers = NetChannel.net2one()
+        def result = fromCheckers.read()
         //println('Got good from: ' + result)
         toReceiver.write(result)
     }
